3de305c58f63281c52836c8631294ecc075d465f
e0 <shan.huang@me.com>
2020-12-10 14:16:23 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mount: {
+    // directory name: 'build directory'
+   public: '/',
+   src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  /* component logic will go here */
+</script>
+<style>
+  /* css will go here */
+</style>
+
+<div>
+  Hello world.
+</div>
